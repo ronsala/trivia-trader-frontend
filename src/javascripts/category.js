32aed17c1568801cf702bf273aa97c5e18137e72
RonSala <ron.sala@gmail.com>
2020-06-13 15:48:57 -0400

    create Category static renderCategories()

--- src/javascripts/category.js
+++ src/javascripts/category.js
@@ -9,15 +9,21 @@ class Category {
   // INDEX
   static renderCategories() {
     document.querySelector('#box-top-p').textContent = 'Q: What category of game do you want to play?';
+    document.querySelector('#box-a').style.display = 'none';
+    document.querySelector('#box-b').style.display = 'none';
 
-    // fetch('http://localhost:3000/api/v1/categories')
-    // .then(response => response.json())
-    // .then(categories => {
-    //   categories.data.forEach(category => {
-    //     let newUser = new User(category, category.attributes);
-    //     document.querySelector('#user-container').innerHTML += newUser.renderUserCard();
-    //   });
-    // });
+    fetch('http://localhost:3000/api/v1/categories')
+    .then(response => response.json())
+    .then(categories => {
+      categories.data.forEach(category => {
+        let newCategory = new Category(category, category.attributes);
+        let div = document.createElement('div');
+        div.className = "box-middle";
+        div.innerHTML = `<p>${newCategory.name}</p>`;
+        div.addEventListener('click', e => console.log(`${newCategory.name} clicked`))
+        document.querySelector('#boxes').appendChild(div);
+      });
+    });
   }
 }
 
