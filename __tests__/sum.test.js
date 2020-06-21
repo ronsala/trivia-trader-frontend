d895234a309c810eef1a8627eaa066474d42898e
RonSala <ron.sala@gmail.com>
2020-06-21 15:44:55 -0400

    wrap test in describe block in __tests__/sum.test.js

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,6 +2,8 @@
 
 const sum = require('../src/javascripts/sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe("Basic math", () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
 });
