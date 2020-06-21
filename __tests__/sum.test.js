fc77d9e920265ec99dae3fd5c31831ce17c504ce
RonSala <ron.sala@gmail.com>
2020-06-21 15:39:30 -0400

    create /__tests__

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,6 +1,6 @@
 // TODO: Remove this test test file when no longer needed.
 
-const sum = require('./sum');
+const sum = require('../src/javascripts/sum');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
