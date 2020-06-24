912692cb68ab2c533c41807afbbfb7e81fd83d17
RonSala <ron.sala@gmail.com>
2020-06-24 17:12:25 -0400

    set up babel

--- test/helpers.js
+++ test/helpers.js
@@ -11,9 +11,9 @@ const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8
 const { JSDOM } = jsdom;
 
 const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', './src/javascripts/index.js')//, {
-    // presets: ['env']
-  // }
+  path.resolve(__dirname, '..', './src/javascripts/user.js')//, {
+//     presets: ['env']
+//   }
 );
 
 const src = babelResult.code
