fb8831ea124516177870d031f6106d01176b87d9
RonSala <ron.sala@gmail.com>
2020-06-24 20:20:36 -0400

    rm jasmine and jest remnants

--- src/javascripts/exampleSong.js
+++
@@ -1,9 +0,0 @@
-// TODO: rm this file (and reference in SpecRunner.html) when no longer needed.
-
-function Song() {
-}
-
-Song.prototype.persistFavoriteStatus = function(value) {
-  // something complicated
-  throw new Error("not yet implemented");
-};
