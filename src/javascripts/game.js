5156fbe4f1b8c67f950956fde404ae40241f7dd4
RonSala <ron.sala@gmail.com>
2020-08-02 15:48:45 -0400

    create answer inputs in loop

--- src/javascripts/game.js
+++ src/javascripts/game.js
@@ -54,6 +54,8 @@ class Game {
   // NEW
   static renderNewForm() {
     window.box_top_p.textContent = "Q: What is your game's info?";
+    window.hero.remove();
+    window.button_home.style.display = 'block';
     window.boxes.remove();
     App.renderBoxes();
 
@@ -93,16 +95,49 @@ class Game {
         label.insertAdjacentElement('afterend', catButton);
         let br = document.createElement('br');
         window.box_category.append(br);
-        f.append(it, window.box_category);        
       });
     });
 
+    // Questions
+    let questionNumbers = ['q1', 'q2', 'q3', 'q4', 'q5'];
     
+    let q1 = document.createElement('input');
+    App.setAttributes(q1, {
+      'id': 'input_q1',
+      'class': 'box-middle',
+      'type': 'text', 
+      'name': 'q1', 
+      'placeholder': "Q: What is the first question?"
+    });
+    let br = document.createElement('br');
+
+    f.append(it, window.box_category, br, q1);
+
+    let answerLetters = ['A', 'B', 'C', 'D'];
+    answerLetters.forEach(answerLetter => {
+      let answer = document.createElement('input');
+      App.setAttributes(answer, {
+        'id': `input_${q1.name}_${answerLetter}`,
+        'class': 'box-middle',
+        'type': 'text', 
+        'name': `${q1.name}_${answerLetter}`, 
+        'placeholder': `Q: What is answer ${answerLetter}?`
+      });
+      f.append(answer);
+    });
+
+    // Answer A
+    // let q1aa = document.createElement('input');
+    // App.setAttributes(q1aa, {
+    //   'id': 'input_q1aa',
+    //   'class': 'box-middle',
+    //   'type': 'text', 
+    //   'name': 'q1aa', 
+    //   'placeholder': "Q: What is answer A?"
+    // });
 
     f.addEventListener('submit', e => { this.handleCreateForm(e);});
     boxes.appendChild(f);
-
-
   }
 }
 
